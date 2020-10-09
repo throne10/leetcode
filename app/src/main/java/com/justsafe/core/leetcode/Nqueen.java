0db9dacdd618fbc41f19a9de47f4e5e32feb64e3
AlanYu <yuxd@justsy.com>
2020-10-09 09:47:19 +0800

    Initial commit

---
+++ app/src/main/java/com/justsafe/core/leetcode/Nqueen.java
@@ -0,0 +1,11 @@
+package com.justsafe.core.leetcode;
+/**
+*
+*@author Yuxd
+*created at 2020/10/9 9:36
+*/
+public class Nqueen {
+    public static void main(String[] s) {
+        System.out.println("1");
+    }
+}
